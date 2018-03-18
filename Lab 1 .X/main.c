1912cdb51b65ac902979e0af70aa9d30523dce11
FF220v <San987873@yandex.ru>
2018-03-18 17:23:49 +0300

    Initial commit

---
+++ Lab 1 .X/main.c
@@ -0,0 +1,5 @@
+#include <xc.h>
+#include <RobotLib.h>
+void main() {
+    
+}
---
+++ RobotLib.X/RobotLib.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   RobotLib.h
+ * Author: GT60
+ *
+ * Created on 18 ????? 2018 ?., 17:18
+ */
+
+#ifndef ROBOTLIB_H
+#define	ROBOTLIB_H
+
+
+
+#endif	/* ROBOTLIB_H */
+
